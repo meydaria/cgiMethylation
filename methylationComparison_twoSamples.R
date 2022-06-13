107c8f7807b035ba94cd68eb23e6b39ed44aa6ff
dmeyer93 <daria.meyer@uni-jena.de>
2022-06-13 12:19:27 +0200

    updated methylation comparison script. Need to give sample names now as 3rd and 4th argument

--- methylationComparison_twoSamples.R
+++ methylationComparison_twoSamples.R
@@ -3,8 +3,8 @@
 ############################################################################################
 ## This script compares the methylation information per CpG island for different samples ##  
 ## Input:                                                                                ## 
-##    - tab-separated file with average methylation per CpG island for control sample    ##
-##    - tab-separated file with average methylation per CpG island for tumor sample      ## 
+##    - 2x tab-separated file with average methylation per CpG island                    ##
+##    - 2x sample name                                                                   ## 
 ##    - optional: output folder                                                          ##
 ## Output:                                                                               ##
 ##    - a plot comparing the average methylation per CpG island                          ##
@@ -12,22 +12,26 @@
 ##      cgi_chro    cgi_start   cgi_end    cgi_id                                        ##
 ############################################################################################
 
+# Rscript methylationComparison_twoSamples.R averageMethylation_ah01_nanopolish_perCGI.txt averageMethylation_t0044c_nanopolish_perCGI.txt ah01 t0044c
+
 # get command line arguments
 args = commandArgs(trailingOnly=TRUE)
 
 
 # test if there is at least one argument: if not, return an error
-if (length(args)<2) {
-  stop("Two input files have to be given: first the control, than the tumor sample", call.=FALSE)
-} else if(length(args)==2){
+if (length(args)<4) {
+  stop("Four input files have to be given: firstly the two methylation files, than the two sample names respectively", call.=FALSE)
+} else if(length(args)==4){
   # set default directory for output files
-  args[3] = getwd()
+  args[5] = getwd()
 }
 
 ## read in the average methylation per CpG island for both sample
 control = read.table(args[1], header = TRUE, sep = "\t")
 tumor = read.table(args[2], header = TRUE, sep = "\t")
-outdir = args[3]
+control.name = args[3]
+tumor.name = args[4]
+outdir = args[5]
 
 ## throw away all CpG isalnds with an coverage <3
 control = control[which(control$coverage>3),]
@@ -37,20 +41,28 @@ tumor = tumor[which(tumor$coverage>3),]
 tumor$coverage <- NULL
 control$coverage <- NULL
 
+colnames(control)[(!colnames(tumor)%in%c("cgi_id", "cgi_chro", "cgi_start", "cgi_end"))] = "control"
+colnames(tumor)[(!colnames(tumor)%in%c("cgi_id", "cgi_chro", "cgi_start", "cgi_end"))] = "tumor"
+
 ## merge the two dataframes
-mergedData = merge(tumor, control, by = c("cgi_id", "cgi_chro", "cgi_start", "cgi_end"))
+mergedData = merge(control, tumor, by = c("cgi_id", "cgi_chro", "cgi_start", "cgi_end"))
+
+print(head(mergedData))
+print(head(mergedData$control.name))
+print(outdir)
 
 ## plot comparison of all CpG islands
-png(paste(outdir, "averageMethylation_plot.png", sep = ""), width = 800, height = 600)
+png(paste(outdir, "/averageMethylation_plot.png", sep = ""), width = 800, height = 600)
 plot(mergedData$control, mergedData$tumor, pch = 16,
-     xlab = "av. methylation control",
-     ylab = "av. methylation tumor",
+     xlab = paste("av. methylation ", control.name, sep = ""),
+     ylab = paste("av. methylation ", tumor.name, sep = ""),
      main = "Average methylation per CpG island")
 dev.off()
 
 ## select interesting CpG islands and write their coordinates to file
-differential = mergedData[which((mergedData$control==0) & (mergedData$tumor>0.7)),]
+differential = mergedData[which(((mergedData$control==0) & (mergedData$tumor>0.7)) | ((mergedData$control>0.7) & (mergedData$tumor==0))),]
+# differential = mergedData[which((mergedData$control==0) & (mergedData$tumor>0.7)),]  # only one way round
 differential_coordinates = differential[,c("cgi_chro", "cgi_start", "cgi_end", "cgi_id")]
-write.table(differential_coordinates, paste(outdir, "differentialMethylation_coordinates.csv", sep = ""), sep = ";", row.names = FALSE, quote = FALSE)
+write.table(differential_coordinates, paste(outdir, "/differentialMethylation_coordinates.csv", sep = ""), sep = ";", row.names = FALSE, quote = FALSE)
 
 
