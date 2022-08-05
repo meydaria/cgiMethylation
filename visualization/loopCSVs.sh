de00f3b491a6087eceb70d1c21f9c9329dd6753f
Daria <daria.meyer@uni-jena.de>
2022-08-05 17:11:28 +0200

    Update loopCSVs.sh

--- visualization/loopCSVs.sh
+++ visualization/loopCSVs.sh
@@ -3,11 +3,12 @@
 # Helper script to loop over all created CSV files and generate the mehtylation plots for them.
 
 folder=$1
+outfolder=$2
 
 files="${folder}/*.csv"
 for f in $files
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  Rscript plotMethylation.R -f $f -o $folder
+  Rscript plotMethylation.R -f $f -o $outfolder
 done
